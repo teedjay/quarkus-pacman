8c42c05668af55c13aad1367347b0aa43bb0e8db
Thore Johnsen <thore.johnsen@evry.com>
2020-11-03 01:00:31 +0100

    Fixed test naming

--- src/test/java/com/teedjay/LevelTest.java
+++ src/test/java/com/teedjay/LevelTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LevelTest {
 
     @Test
-    public void verifyTimeMap() {
+    public void verifyTileMapTest() {
 
         Level level = new Level();
         
